359e7ede3bbb6f1d22af806d2e79dfb98c4da825
Safia Abdalla <safia@microsoft.com>
2020-04-29 15:03:52 -0700

    Move Blazor Server template tests to new project

--- src/ProjectTemplates/BlazorTemplates.Tests/BlazorServerTemplateTest.cs
+++ src/ProjectTemplates/BlazorTemplates.Tests/BlazorServerTemplateTest.cs
@@ -25,7 +25,6 @@ namespace Templates.Test
         public Project Project { get; private set; }
 
         [ConditionalFact(Skip = "This test ran for over an hour")]
-        [SkipOnHelix("selenium")]
         [QuarantinedTest("https://github.com/dotnet/aspnetcore/issues/20172")]
         public async Task BlazorServerTemplateWorks_NoAuth()
         {
@@ -84,7 +83,6 @@ namespace Templates.Test
         [ConditionalTheory(Skip = "See: https://github.com/dotnet/aspnetcore/issues/20520")]
         [InlineData(true)]
         [InlineData(false)]
-        [SkipOnHelix("ef restore no worky")]
         [QuarantinedTest]
         public async Task BlazorServerTemplateWorks_IndividualAuth(bool useLocalDB)
         {
---
+++ src/ProjectTemplates/BlazorTemplates.Tests/Helpers/README.md
@@ -0,0 +1,1 @@
+The sources contained in this folder are copied from [../../test/Helpers/](the original test helpers).
